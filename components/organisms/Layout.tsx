6cd252cb8dbfb68394b8bea7b5d0767cfab3e983
Osmond <osmond@heysparkbox.com>
2022-04-11 13:19:36 -0600

    feat: Add favicon
    References FSA22V1-183

--- components/organisms/Layout.tsx
+++ components/organisms/Layout.tsx
@@ -7,6 +7,9 @@ const Layout: React.FC<{}> = ({ children }) => {
   return (
     <div>
       <Head>
+        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
         <title>mealwise</title>
       </Head>
 
