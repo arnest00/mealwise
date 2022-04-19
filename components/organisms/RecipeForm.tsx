f4f643c3b3d16ec3d2b14aa2d28cc6a87dc1f4f1
Osmond <osmond@heysparkbox.com>
2022-04-19 13:20:43 -0600

    feat: Save recipe id, name, and category to Indexed DB
    References FSA22V1-185

--- components/organisms/RecipeForm.tsx
+++ components/organisms/RecipeForm.tsx
@@ -1,3 +1,7 @@
+import { useState } from 'react';
+import { nanoid } from 'nanoid';
+import { db } from '../../lib/db';
+
 import InputGroup from '../atoms/InputGroup';
 import SelectGroup from '../atoms/SelectGroup';
 import Button from '../atoms/Button';
@@ -5,8 +9,33 @@ import Button from '../atoms/Button';
 import IngredientInputs from '../molecules/IngredientInputs';
 
 const RecipeForm = () => {
+  const [ status, setStatus ] = useState("");
+
+  const addRecipe = async (e) => {
+    e.preventDefault();
+    
+    const form = e.currentTarget;
+    
+    try {
+      const formData = new FormData(form);
+      const formDataObject = Object.fromEntries(formData.entries());
+
+      const newRecipe = {
+        id: nanoid(),
+        name: formDataObject['recipe name'],
+        category: formDataObject['category'],
+      };
+
+      await db.recipes.add(newRecipe);
+
+      setStatus(`Recipe "${newRecipe.name}" added!`);
+    } catch (err) {
+      console.error(err);
+    };
+  };
+
   return (
-    <form className="form">
+    <form className="form" onSubmit={addRecipe}>
       <fieldset className="form__fieldset">
         <legend className="bigger">Recipe Information</legend>
 
@@ -70,6 +99,8 @@ const RecipeForm = () => {
           modifier='--bad-job'
         />
       </div>
+
+      <p>{status}</p>
     </form>
   );
 };
