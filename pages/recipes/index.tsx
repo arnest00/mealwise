2f995e36f8ae48e0128e1a8174c889d4585a4a10
Osmond <osmond@heysparkbox.com>
2022-04-26 15:52:19 -0600

    feat: Render saved recipe data from Indexed DB
    References 186

--- pages/recipes/index.tsx
+++ pages/recipes/index.tsx
@@ -1,10 +1,40 @@
+import { useEffect, useState } from 'react';
 import { NextPage } from 'next';
 import Link from 'next/link';
+import { getAllRecipes } from '../../services/dbService';
 
 import Layout from '../../components/organisms/Layout';
 import RecipeBookCategory from '../../components/organisms/RecipeBookCategory';
 
 const RecipesPage: NextPage = () => {
+  const [ recipes, setRecipes ] = useState({
+    breakfast: [],
+    lunch: [],
+    dinner: []
+  });
+
+  useEffect(() => {
+    const getAndSetData = async () => {
+      try {
+        const allRecipes = await getAllRecipes();
+
+        const breakfastRecipes = allRecipes.filter((recipe) => recipe.category === 'Breakfast');
+        const lunchRecipes = allRecipes.filter((recipe) => recipe.category === 'Lunch');
+        const dinnerRecipes = allRecipes.filter((recipe) => recipe.category === 'Dinner');
+
+        const newRecipesState = {
+          breakfast: [...breakfastRecipes],
+          lunch: [...lunchRecipes],
+          dinner: [...dinnerRecipes],
+        };
+        
+        setRecipes(newRecipesState);
+      } catch (err) {};
+    };
+
+    getAndSetData();
+  });
+
   return (
     <Layout>
       <h1 className="title text-align-center">Recipe Book</h1>
@@ -17,14 +47,17 @@ const RecipesPage: NextPage = () => {
 
       <RecipeBookCategory
         categoryName='breakfast'
+        categoryRecipes={recipes.breakfast}
       />
 
       <RecipeBookCategory
         categoryName='lunch'
+        categoryRecipes={recipes.lunch}
       />
 
       <RecipeBookCategory
         categoryName='dinner'
+        categoryRecipes={recipes.dinner}
       />
     </Layout>
   );
