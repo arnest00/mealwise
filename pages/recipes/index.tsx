f8c652469883647521d1411bf1e6196117e5faad
Osmond <osmond@heysparkbox.com>
2022-04-26 14:29:04 -0600

    chore: Remove hard-coded mock data
    References 186

--- pages/recipes/index.tsx
+++ pages/recipes/index.tsx
@@ -5,32 +5,6 @@ import Layout from '../../components/organisms/Layout';
 import RecipeBookCategory from '../../components/organisms/RecipeBookCategory';
 
 const RecipesPage: NextPage = () => {
-  const seedBreakfastRecipes = [
-    {
-      name: 'Breakfast of Champions',
-      description: 'A Red Bull and a cigarette.'
-    },
-    {
-      name: 'Scrambled Eggs',
-      description: 'A deconstructed, crustless quiche.'
-    },
-    {
-      name: 'Fried Chicken and Waffles',
-      description: 'When you have time to cook dinner and breakfast at the same time.'
-    },
-  ];
-  
-  const seedDinnerRecipes = [
-    {
-      name: 'Toast Sandwich',
-      description: 'A slice of bread between two slices of bread.'
-    },
-    {
-      name: 'Busy Day at Work',
-      description: 'The number to the local Chinese takeout place is 555-0106.'
-    },
-  ];
-
   return (
     <Layout>
       <h1 className="title text-align-center">Recipe Book</h1>
@@ -43,7 +17,6 @@ const RecipesPage: NextPage = () => {
 
       <RecipeBookCategory
         categoryName='breakfast'
-        categoryRecipes={seedBreakfastRecipes}
       />
 
       <RecipeBookCategory
@@ -52,7 +25,6 @@ const RecipesPage: NextPage = () => {
 
       <RecipeBookCategory
         categoryName='dinner'
-        categoryRecipes={seedDinnerRecipes}
       />
     </Layout>
   );
