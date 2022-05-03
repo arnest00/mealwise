1233f51d61ad2cec035b1bbd72b0b11ae851e71e
Osmond <osmond@heysparkbox.com>
2022-05-03 12:11:07 -0600

    feat: Expand `IRecipe` to include all expected properties
    References 131

--- interfaces/IRecipe.ts
+++ interfaces/IRecipe.ts
@@ -1,6 +1,11 @@
+import IIngredient from "./IIngredient";
+
 export default interface IRecipe {
   id: string,
   name: string,
+  description?: string,
+  link?: string,
   category: string,
-  description: string
+  servings?: number,
+  ingredients: IIngredient[],
 };
