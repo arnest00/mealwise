0d26c56bca82392c67c10874a67403970568d498
Osmond <osmond@heysparkbox.com>
2022-05-04 14:21:13 -0600

    feat: Add reroute action to button on RecipeCard component
    References 211

--- components/molecules/RecipeCard.tsx
+++ components/molecules/RecipeCard.tsx
@@ -1,20 +1,32 @@
+import { useRouter } from 'next/router';
+
 import IRecipe from '../../interfaces/IRecipe';
+
 import Button from '../atoms/Button';
 
-const RecipeCard = ({ name, description }: IRecipe) => (
-  <article className="card">
-    <h3 className="bigger card__title">{name}</h3>
+const RecipeCard = ({ name, description, id }: IRecipe) => {
+  const router = useRouter();
 
-    <p className="card__description">
-      {description}
-    </p>
+  const handleClick = () => {
+    router.push(`/recipes/${id}`);
+  };
 
-    <Button
-      buttonType="button"
-      buttonName="view recipe"
-      modifier="--link"
-    />
-  </article>
-);
+  return (
+    <article className="card">
+      <h3 className="bigger card__title">{name}</h3>
+
+      <p className="card__description">
+        {description}
+      </p>
+
+      <Button
+        buttonType="button"
+        buttonName="view recipe"
+        modifier="--link"
+        onClick={handleClick}
+      />
+    </article>
+  );
+};
 
 export default RecipeCard;
