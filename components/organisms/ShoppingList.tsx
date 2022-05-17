23830bd2945410161d1c8018bde7a58d72e91e8c
Osmond <osmond@heysparkbox.com>
2022-05-17 15:04:26 -0600

    feat: Create ShoppingList component with hard-coded items
    References 130

---
+++ components/organisms/ShoppingList.tsx
@@ -0,0 +1,11 @@
+const ShoppingList = () => (
+  <article>
+    <ul>
+      <li>Bread</li>
+      <li>Milk</li>
+      <li>Eggs</li>
+    </ul>
+  </article>
+);
+
+export default ShoppingList;
--- pages/shopping-list/index.tsx
+++ pages/shopping-list/index.tsx
@@ -1,12 +1,15 @@
 import { NextPage } from 'next';
 
 import Layout from '../../components/organisms/Layout';
+import ShoppingList from '../../components/organisms/ShoppingList';
 
 const ShoppingListPage: NextPage = () => (
   <Layout>
     <h1 className="title text-align-center">
       Shopping List
     </h1>
+
+    <ShoppingList />
   </Layout>
 );
 
